a7bb4baf025aa38b054a25fe6a8e41a14691f2f4
muhammadkashaf <kashafahmed72@gmail.com>
2025-04-17 15:11:22 +0500

    small change

--- src/index.js
+++ src/index.js
@@ -23,8 +23,6 @@ async function checkForNewVersion() {
     console.log('Meta Version:', meta.version);
 
     if (meta.version !== CURRENT_VERSION) {
-      console.log('🔁 New version detected, reloading...');
-      location.reload(true); // force hard refresh
       notifyUpdateAvailable();
     }
   } catch (err) {
